ecf5e97d025ec39f42ef3b25a77aab0137c51754
Keyner de Avila <kddeavila3@gmail.com>
2025-10-27 14:53:29 -0500

    feat: Add drag indicator to ruler markers and refactor constants
    - Add vertical blue line on marker drag for better UX
    - Refactor ruler constants (PAGE_WIDTH, MINIMUM_SPACE) and fix className typo
    - Apply code formatting with Biome

--- src/extensions/font-size.ts
+++ src/extensions/font-size.ts
@@ -2,56 +2,58 @@ import { Extension } from "@tiptap/react";
 import "@tiptap/extension-text-style";
 
 declare module "@tiptap/core" {
-    interface Commands<ReturnType> {
-        fontSize: {
-            setFontSize: (size: string) => ReturnType;
-            unsetFontSize: () => ReturnType;
-        };
-    }
+  interface Commands<ReturnType> {
+    fontSize: {
+      setFontSize: (size: string) => ReturnType;
+      unsetFontSize: () => ReturnType;
+    };
+  }
 }
 
 export const FontSizeExtension = Extension.create({
-    name: "fontSize",
-    addOptions() {
-        return {
-            types: ["textStyle"]
-        }
-    },
-    addGlobalAttributes() {
-        return [
-            {
-                types: this.options.types,
-                attributes: {
-                    fontSize: {
-                        default: null,
-                        parseHTML: element => element.style.fontSize,
-                        renderHTML: attributes => {
-                            if (!attributes.fontSize) {
-                                return {}
-                            }
+  name: "fontSize",
+  addOptions() {
+    return {
+      types: ["textStyle"],
+    };
+  },
+  addGlobalAttributes() {
+    return [
+      {
+        types: this.options.types,
+        attributes: {
+          fontSize: {
+            default: null,
+            parseHTML: (element) => element.style.fontSize,
+            renderHTML: (attributes) => {
+              if (!attributes.fontSize) {
+                return {};
+              }
 
-                            return {
-                                style: `font-size: ${attributes.fontSize}`
-                            }
-                        }
-                    }
-                }
-            }
-        ]
-    },
-    addCommands() {
-        return {
-            setFontSize: (fontSize: string) => ({ chain }) => {
-                return chain()
-                    .setMark("textStyle", { fontSize })
-                    .run()
+              return {
+                style: `font-size: ${attributes.fontSize}`,
+              };
             },
-            unsetFontSize: () => ({ chain }) => {
-                return chain()
-                    .setMark("textStyle", { fontSize: null })
-                    .removeEmptyTextStyle()
-                    .run()
-            }
-        }
-    }
+          },
+        },
+      },
+    ];
+  },
+  addCommands() {
+    return {
+      setFontSize:
+        (fontSize: string) =>
+        ({ chain }) => {
+          return chain().setMark("textStyle", { fontSize }).run();
+        },
+      unsetFontSize:
+        () =>
+        ({ chain }) => {
+          return chain()
+            .setMark("textStyle", { fontSize: null })
+            .removeEmptyTextStyle()
+            .run();
+        },
+    };
+  },
 });
